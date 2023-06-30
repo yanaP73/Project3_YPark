a25922ef844c966557a33e538509aafdbb592a3f
Yana-Park <yanna73kr@gmail.com>
2023-06-30 21:17:32 +0100

    Create borders around avatar and thumbnails, and add a change color of the border to orange on hover

---
+++ Project_3.js
@@ -0,0 +1,21 @@
+/*$(document).ready(function() {
+    $("#avatar").on("click", function() {
+       
+    });
+});*/
+
+/*$(document).ready(function() {
+    $("href").on({
+        "mouseover": function(){
+            if($(this).id("avatar").match(/index.html/)){
+                alert("This is avatar!")
+        }
+          },
+    "mouseover" : function(){
+        if($(this).id("thumbnail").match(/index.html/)){
+            alert("This is thumnail!")
+        }
+    }
+    });
+});
+*/
