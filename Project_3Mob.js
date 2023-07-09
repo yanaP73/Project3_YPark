e62af66dc4b27e5731ebd695276352395a091785
Yana-Park <yanna73kr@gmail.com>
2023-07-09 01:55:57 +0100

    Add hamburger menu with animated side menu. Change padding, margine of menu icon and logo. Put cart and avatar on the same line as menu icon and logo. Add java script function to open and close the menu.

--- Project_3Mob.js
+++ Project_3Mob.js
@@ -19,3 +19,10 @@
     });
 });
 */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
